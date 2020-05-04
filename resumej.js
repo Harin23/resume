c8e6f54dbb102a47d5c9a3bda3dc5cace80c8256
Harin23 <harin.s.bal@gmail.com>
2020-05-04 15:27:28 -0600

    second commit

--- resumej.js
+++ resumej.js
@@ -1,6 +1,14 @@
-function skillSearch(){
-var user_i, user_if;
-user_i = document.getElementById('input-skill-search');
-user_if = user_i.value.toUpperCase();
-console.log(user_if);
+console.log("js file loaded")
+function search_skill(){
+var user_i;
+user_i = document.getElementById("skillInput").value;
+console.log(user_i);
+//user_i = detect_and_uppercase_fl(user_i);
+//user_i = detect_space_and_uppercase_fl(user_i);
+//console.log(user_i);
 }
+
+//var b = user_i[0] != user_i[0].toupperCase()
+   // console.log(b)
+
+
