743e5e48030fdca7b5af1ae47768b0c53c9b2e47
Harin23 <harin.s.bal@gmail.com>
2020-05-04 23:36:31 -0600

    fixed the search bar in the skills section

--- resumej.js
+++ resumej.js
@@ -1,34 +1,22 @@
-console.log("js file loaded")
+
 function search_skill(){
-var user_i, user_if;
-user_i = document.getElementById("skillInput").value;
-user_if = detect_and_uppercase_fl(user_i);
-//user_i = detect_space_and_uppercase_fl(user_i);
-console.log(user_if);
-
-}
-
-function detect_and_uppercase_fl(a){
-   var user_i, ucl, user_in;
-   user_i=a; 
-   if ( (user_i[0]) !== (user_i[0].toUpperCase()) ){
-      ucl=user_i[0].toUpperCase()
-      console.log(ucl);
-      user_in = reconstruct_string(user_i, ucl)
-   }
-   return user_in;
-}
-
-function reconstruct_string(str, letter){
-   var user_input, l, capital_l, new_string;
-   user_input = str; capital_l = letter;
-   l=user_input.length;
+   var user_i, user_if, l, table, tr, td, i, j, total_col, skill;
+   user_i = document.getElementById("skillInput").value;
+   l=user_i.length;
+   table = document.getElementById("skills-table");
+   tr = table.getElementsByTagName("tr");
+   total_col = document.getElementById("skills-table").rows[0].cells.length;
    
-   if (l >1){
-      new_string = capital_l + user_input.substr(1,l-1);
+   for (i=0; i < tr.length; i++){
+      for (j=0; j < total_col; j++){
+         td=tr[i].getElementsByTagName("td")[j];
+         skill = td.textContent;
+         if(skill.substr(0,l).toLowerCase() != user_i.toLowerCase()){
+            td.style.display = "none";
+         }else{
+            td.style.display = "";
+         }
+      }
    }
-
-   return new_string;
 }
-
 
